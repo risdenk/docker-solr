5e0d981017fcc02888931eb2f7862e03610c8328
Kevin Risden <risdenk@avalonconsult.com>
2016-02-25 09:16:31 -0600

    Fix error messages

--- update_trunk.sh
+++ update_trunk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 set -e -o pipefail
+set -x 
 
 TEMPLATE=Dockerfile.template.trunk
 BUILD_NUMBER=$(curl -sSL -f https://builds.apache.org/job/Solr-Artifacts-trunk/lastSuccessfulBuild/buildNumber)
 SOLR_VERSION="6.0.0-$BUILD_NUMBER"
 
-set -x
 cp $TEMPLATE "trunk/Dockerfile"
 sed -r -i -e 's/^(ENV SOLR_VERSION) .*/\1 '"$SOLR_VERSION"'/' "trunk/Dockerfile"
 
