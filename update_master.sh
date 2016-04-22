59a6f07e173a1afcf3f0497d26dc590ed33403f1
Kevin Risden <risdenk@avalonconsult.com>
2016-04-22 09:03:12 -0500

    Update scripts

--- update_master.sh
+++ update_master.sh
@@ -3,12 +3,18 @@
 set -e -o pipefail
 set -x 
 
-TEMPLATE=Dockerfile.template.master
 BUILD_NUMBER=$(curl -sSL -f https://builds.apache.org/job/Solr-Artifacts-master/lastSuccessfulBuild/buildNumber)
 SOLR_VERSION="7.0.0-$BUILD_NUMBER"
 
-cp $TEMPLATE "master/Dockerfile"
+rm -rf master/*
+cp -r scripts docker-entrypoint.sh master/
+cp Dockerfile.template.master "master/Dockerfile"
 sed -r -i -e 's/^(ENV SOLR_VERSION) .*/\1 '"$SOLR_VERSION"'/' "master/Dockerfile"
+
+mkdir -p master/alpine
+cp -r scripts docker-entrypoint.sh master/alpine/
+cp Dockerfile-alpine.template.master "master/alpine/Dockerfile"
+sed -r -i -e 's/^(ENV SOLR_VERSION) .*/\1 '"$SOLR_VERSION"'/' "master/alpine/Dockerfile"
 
 git add master
 git commit -S -m "Updated Solr master to $SOLR_VERSION"
