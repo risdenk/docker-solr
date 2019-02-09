23f461880c7c4e4c5f8394f04c249932876546ed
Kevin Risden <krisden@apache.org>
2019-02-09 09:49:51 -0500

    Update master script

--- update_master.sh
+++ update_master.sh
@@ -4,17 +4,17 @@ set -e -o pipefail
 set -x 
 
 BUILD_NUMBER=$(curl -sSL -f https://builds.apache.org/job/Solr-Artifacts-master/lastSuccessfulBuild/buildNumber)
-SOLR_VERSION="8.0.0-$BUILD_NUMBER"
+SOLR_VERSION="9.0.0-$BUILD_NUMBER"
 
 rm -rf master/*
 cp -r scripts master/
 cp Dockerfile.template.master "master/Dockerfile"
-sed -r -i -e 's/^(ENV SOLR_VERSION) .*/\1 '"$SOLR_VERSION"'/' "master/Dockerfile"
+gsed -r -i -e 's/^(ENV SOLR_VERSION) .*/\1 '"$SOLR_VERSION"'/' "master/Dockerfile"
 
 mkdir -p master/alpine
 cp -r scripts master/alpine/
 cp Dockerfile-alpine.template.master "master/alpine/Dockerfile"
-sed -r -i -e 's/^(ENV SOLR_VERSION) .*/\1 '"$SOLR_VERSION"'/' "master/alpine/Dockerfile"
+gsed -r -i -e 's/^(ENV SOLR_VERSION) .*/\1 '"$SOLR_VERSION"'/' "master/alpine/Dockerfile"
 
 git add master
 git commit -S -m "Updated Solr master to $SOLR_VERSION"
